2269f65a2de6fbb4fb10c1907b442cb2b0a9fe7e
matthewinscore <matthewinscore@gmail.com>
2025-06-03 11:15:36 -0700

    Finished binenv v1, created pygame loop

--- BinPackingEnv.py
+++ BinPackingEnv.py
@@ -1,16 +1,21 @@
 from typing import Tuple
 import math
+import pygame
 
 class BinPackingEnv():
+    RW_VALID = 1 # reward given for valid placement
+    PN_INVALID = -1 # penalty for invalid placement (scaled on how much overlap?)
+
     def __init__(self, circles: list):
         self.circles = circles # List of circle radiis. ex: [r1, r2, r3]
         self.placed_circles = list() # List of positions of placed circles and their radii. ex: [(x, y), r]
-        self.circles_left = self.circles - self.placed_circles
+        self.circles_left = len(self.circles) - len(self.placed_circles)
         self.num_circles = len(circles)
         self.num_circles_left = len(self.circles)
         self.current_index = 0
         self.done = False
         print("Bin initialized.")
+
 
     def reset(self) -> Tuple[list, int]:
         # Empties the bin and resets index, returns the new (empty) state.
@@ -24,12 +29,26 @@ class BinPackingEnv():
         return self.placed, self.index
     
     def step(self, action: tuple) -> Tuple[list, int, int, type]:
+        # Takes in the action (position). ex: action = (x, y)
         # Returns the next state, reward/penalty, and status, based on the validity of the action.
-        pass
+        reward = 0
+        if self._is_valid_placement(action, self.circles[self.index]):
+            reward += self.RW_VALID
+        else:
+            reward += self.PN_INVALID
+        
+        self.index += 1
+        if self.index == self.num_circles:
+            self.done = True
+
+        return self._get_state(), reward, self.done
+
 
     def _is_valid_placement(self, position, radius) -> type:
         # Determines validity of position with radius given already packed circles.
+        if position[0] < radius or position[1] < radius:
+            return False
         for taken_pos, taken_r in self.placed_circles:
-            if math.dist(taken_pos, position) <= taken_r + r:
+            if math.dist(taken_pos, position) <= taken_r + radius:
                 return False
         return True
--- main.py
+++ main.py
@@ -1,4 +1,55 @@
 from BinPackingEnv import BinPackingEnv
+import pygame
+
+pygame.init()
+pygame.display.set_caption("2d Bin Circle Packing Simulation")
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((1560, 900))
+
+
 
 circles = [20, 20, 10, 30, 20]
 bin = BinPackingEnv(circles)
+
+
+def update_display(): # regular display updater
+    screen.fill((0, 0, 0))
+
+
+# loop variables
+started = False # status of running the simulation
+
+# main loop
+update_display()
+running = True
+while running:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and not started:
+            pass
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q: # quit program
+                running = False
+
+            elif event.key == pygame.K_DELETE or event.key == pygame.K_c: # clear graph
+                pass
+                started = False
+            
+            elif (event.key == pygame.K_RETURN or event.key == pygame.K_s) and not started: # start simulation
+                pass 
+                started = False
+
+            elif event.key == pygame.K_t: # for testing
+                print("test started")
+
+    #update_display()
+    pygame.display.flip()
+
+pygame.quit()
+
