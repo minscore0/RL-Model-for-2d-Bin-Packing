b00d7c607538a13fdf9513cc7fc049409ae7b410
minscore0 <matthewinscore@gmail.com>
2025-06-05 06:11:17 -0700

    Implemented pygame loop. Changed some class structure.

--- BinPackingEnv.py
+++ BinPackingEnv.py
@@ -6,29 +6,27 @@ class BinPackingEnv():
     RW_VALID = 1 # reward given for valid placement
     PN_INVALID = -1 # penalty for invalid placement (scaled on how much overlap?)
 
-    def __init__(self, circles: list):
-        self.circles = circles # List of circle radiis. ex: [r1, r2, r3]
-        self.placed_circles = list() # List of positions of placed circles and their radii. ex: [(x, y), r]
-        self.circles_left = len(self.circles) - len(self.placed_circles)
-        self.num_circles = len(circles)
-        self.num_circles_left = len(self.circles)
-        self.current_index = 0
+    def __init__(self, circle_radii: list):
+        self.circle_radii = circle_radii # List of circle radii. ex: [r1, r2, r3]
+        self.circles = [[r, [0, 0]] for r in self.circle_radii] # List of positions of placed circles and their radii. ex: [r, [x, y]]
+        self.num_circles = len(self.circle_radii)
+        self.index = 0
+        self.placed_circles = self.circles[:self.index]
         self.done = False
         print("Bin initialized.")
 
-
     def reset(self) -> Tuple[list, int]:
         # Empties the bin and resets index, returns the new (empty) state.
-        self.placed_circles = list()
-        self.current_index = 0
+        self.placed_circles = [[r, [0, 0]] for r in self.circle_radii]
+        self.index = 0
         self.done = False
         return self._get_state()
     
     def _get_state(self) -> Tuple[list, int]:
-        # returns the state (positions of placed circles, index)
-        return self.placed, self.index
+        # returns the state (positions, index)
+        return self.circles, self.index
     
-    def step(self, action: tuple) -> Tuple[list, int, int, type]:
+    def step(self, action: tuple) -> tuple[tuple[list, int], int, bool]:
         # Takes in the action (position). ex: action = (x, y)
         # Returns the next state, reward/penalty, and status, based on the validity of the action.
         reward = 0
@@ -36,15 +34,15 @@ class BinPackingEnv():
             reward += self.RW_VALID
         else:
             reward += self.PN_INVALID
-        
+
+        self.circles[self.index][1] = action
         self.index += 1
         if self.index == self.num_circles:
             self.done = True
 
         return self._get_state(), reward, self.done
 
-
-    def _is_valid_placement(self, position, radius) -> type:
+    def _is_valid_placement(self, position, radius) -> bool:
         # Determines validity of position with radius given already packed circles.
         if position[0] < radius or position[1] < radius:
             return False
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from BinPackingEnv import BinPackingEnv
 import pygame
+import pygame.gfxdraw
 
 pygame.init()
 pygame.display.set_caption("2d Bin Circle Packing Simulation")
@@ -7,20 +8,23 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1560, 900))
 
 
+circle_radii = [20, 20, 10, 30, 20]
+packing_bin = BinPackingEnv(circle_radii)
 
-circles = [20, 20, 10, 30, 20]
-bin = BinPackingEnv(circles)
 
-
-def update_display(): # regular display updater
+def update_display(screen, circles, index):  # regular display updater
     screen.fill((0, 0, 0))
+    for r, pos in circles[:index]:
+        pygame.gfxdraw.circle(screen, 100+pos[0], 800-pos[1], r, (255, 255, 255))
+
+    return None
 
 
-# loop variables
-started = False # status of running the simulation
+# active variables
+started = False  # status of running the simulation
 
 # main loop
-update_display()
+update_display(screen, packing_bin.circles, packing_bin.index)
 running = True
 while running:
 
@@ -34,21 +38,21 @@ while running:
             pass
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q: # quit program
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:  # quit program
                 running = False
 
-            elif event.key == pygame.K_DELETE or event.key == pygame.K_c: # clear graph
+            elif event.key == pygame.K_DELETE or event.key == pygame.K_c:  # clear graph
                 pass
                 started = False
             
-            elif (event.key == pygame.K_RETURN or event.key == pygame.K_s) and not started: # start simulation
+            elif (event.key == pygame.K_RETURN or event.key == pygame.K_s) and not started:  # start simulation
                 pass 
                 started = False
 
-            elif event.key == pygame.K_t: # for testing
+            elif event.key == pygame.K_t:  # for testing
                 print("test started")
 
-    #update_display()
+    update_display(screen, packing_bin.circles, packing_bin.index)
     pygame.display.flip()
 
 pygame.quit()
